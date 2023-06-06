de7394aa00ba67f62bf908807df68dafddc74ab8
AydanKusland <chinatugov@mail.ru>
2023-06-06 11:43:38 +0800

    More shades now!

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Values from 'values.js'
 import { ToastContainer, toast } from 'react-toastify'
 
 const defaultColor = '#811fd1'
+const shadeStep = 5
 
 const App = () => {
-	const [colorList, setColorList] = useState(new Values(defaultColor).all(10))
+	const [colorList, setColorList] = useState(
+		new Values(defaultColor).all(shadeStep).slice(1, 200 / shadeStep)
+	)
 
 	const checkColor = color => {
 		if (!color.startsWith('#')) {
 			toast.error('Color value should start with #!')
 			return false
 		}
-		if (color.length < 7) {
-			toast.error('Color should has 6 symbols after the # sign!')
+		if (color.length < 4) {
+			toast.error('Color should has at least 3 symbols after the # sign!')
 			return false
 		}
 		return true
@@ -25,7 +28,9 @@ const App = () => {
 		const color = e.target[1].value
 		try {
 			if (!checkColor(color)) return
-			const newColorList = new Values(color).all(10)
+			const newColorList = new Values(color)
+				.all(shadeStep)
+				.slice(1, 200 / shadeStep)
 			setColorList(newColorList)
 		} catch (error) {
 			toast.error(error.message)
